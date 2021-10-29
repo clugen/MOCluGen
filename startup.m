be80fccffc0ca4e72865d801d2ed16b38785177b
Nuno Fachada <faken@fakenmc.com>
2021-10-29 20:34:48 +0100

    Add initialization startup files

---
+++ startup.m
@@ -0,0 +1,9 @@
+% Run this script to initialize MOCluGen.
+%
+% Copyright (c) 2013-2021 Nuno Fachada
+% Distributed under the MIT License (See accompanying file LICENSE or copy
+% at http://opensource.org/licenses/MIT)
+%
+
+% Add path containing source code
+addpath([pwd() filesep 'src']);
