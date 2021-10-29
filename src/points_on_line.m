0d47d833e9effe341064eab1d5c5dc9568b88756
Nuno Fachada <faken@fakenmc.com>
2021-10-29 20:50:01 +0100

    Add doccomment to points_on_line()

--- src/points_on_line.m
+++ src/points_on_line.m
@@ -1,8 +1,18 @@
-% Determine coordinates of points on a line with `center` and `direction`, based
-% on the distances from the center given in `dist_center`.
-%
-% This works by using the parametric line equation assuming `direction` is normalized.
 function points = points_on_line(center, direction, dist_center)
+% POINTS_ON_LINE Determine coordinates of points on a line with `center` and
+% `direction`, based on the distances from the center given in `dist_center`.
+%
+% points = POINTS_ON_LINE(center, direction, dist_center)
+%
+% This works by using the vector formulation of the line equation assuming
+% `direction` is a n-dimensional unit vector.
+%
+% ## Arguments
+%
+% * `center` - Center of the line (n x 1 vector).
+% * `direction` - Line direction (n x 1 unit vector).
+% * `dist_center` - Distance of each point to the center of the line (p x 1
+%   vector, where p is the number of points).
 
     points = center' + dist_center * direction';
 
