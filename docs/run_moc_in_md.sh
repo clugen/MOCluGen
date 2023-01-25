0bf1a0251f4a7eb040223ce2c6f0c3b79c3babff
Nuno Fachada <faken@fakenmc.com>
2023-01-25 15:20:39 +0000

    Docs: make sure example plots build headless

--- docs/run_moc_in_md.sh
+++ docs/run_moc_in_md.sh
@@ -9,20 +9,32 @@
 
 # Check number of parameters
 if [[ $# -ne 1 ]]; then
-    echo "Usage: $0 <MARKDOWN_FILE>"
+    >&2 echo "Usage: $0 <MARKDOWN_FILE>"
     exit 1
 fi
 
 # Check if file exists
 if ! test -f $1; then
-    echo "File '$1' does not exist"
+    >&2 echo "File '$1' does not exist"
     exit 2
 fi
 
 # Check if Octave is found
 if ! command -v octave &> /dev/null; then
-    echo "GNU Octave could not be found"
+    >&2 echo "GNU Octave could not be found"
     exit 3
+fi
+
+# Check if a physical display is not available
+if [[ -z "${DISPLAY}" ]]; then
+    # Check if a virtual display is available via xvfb
+    if command -v xvfb-run &> /dev/null; then
+        # We have a virtual display, so plots will turn out nice
+        XVFB_RUN="xvfb-run"
+    else
+        # We also don't have a virtual display, plots will probably not be good
+        >&2 echo "No physical or virtual display found, plots may not look good"
+    fi
 fi
 
 # If we get here we can extract and run the embedded code
@@ -31,7 +43,7 @@ pkg load statistics
 addpath([pwd() filesep '..' filesep 'src']);
 addpath([pwd() filesep 'plot_funcs']);
 $(./md2moc.py $1)
-close all hidden" | octave
+close all hidden" | $(echo ${XVFB_RUN}) octave
 
 # Move the generated images to docs/img
 mv *.png docs/img
