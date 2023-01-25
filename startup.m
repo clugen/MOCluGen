5558dc42b36f3c22be4a43cb93d1465c9f0a8cd3
Nuno Fachada <faken@fakenmc.com>
2023-01-25 17:04:52 +0000

    Define default levels of testing in startup.m

--- startup.m
+++ startup.m
@@ -1,9 +1,21 @@
 % Run this script to initialize MOCluGen.
 %
-% Copyright (c) 2013-2021 Nuno Fachada
+% Copyright (c) 2013-2023 Nuno Fachada
 % Distributed under the MIT License (See accompanying file LICENSE or copy
 % at http://opensource.org/licenses/MIT)
 %
 
 % Add path containing source code
 addpath([pwd() filesep 'src']);
+
+% Global variable, which defines the testing mode
+global moclugen_test_mode
+
+% Global variable can be set to 'minimal', 'ci', 'normal', 'full'
+% If running on a CI environment, it's set to 'ci'
+% Otherwise it's set to 'normal'
+if numel(getenv('CI')) > 0
+    moclugen_test_mode = 'ci';
+else
+    moclugen_test_mode = 'normal';
+end;
