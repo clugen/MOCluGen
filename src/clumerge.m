410b1674d08b36c9d9979a0b6c6ebd67b6bb6cbf
Nuno Fachada <faken@fakenmc.com>
2023-06-26 15:59:10 +0100

    clumerge(): half-way through implementation

--- src/clumerge.m
+++ src/clumerge.m
@@ -2,54 +2,161 @@ function mrgdata = clumerge(datasets, varargin)
 
     % Setup input validation
     p = inputParser;
-    
+
     % Check that datasets is a cell with at least one dataset
     addRequired(p, 'datasets', @(x) iscell(x) && numel(x) > 0);
-    
+
     % Check that fields is a cell with at least one field
     addParameter(p, 'fields', {'points', 'clusters'}, ...
         @(x) iscell(x) && numel(x) > 0);
-    
+
     % Check that clusters_field is a string
     addParameter(p, 'clusters_field', 'clusters', @(x) isstring(x));
-    
-    % Check that output_type is either 'struct' or 'map'
-    addParameter(p, 'output_type', 'struct', ...
-        @(x) isstring(x) && any(validatestring(x, {'struct', 'map'})));
 
     % Perform input validation and parsing
     parse(p, datasets, varargin{:});
-    
+
     % Number of elements in each array the merged dataset
     nelems = 0;
 
     % Contains information about each field
-    fields_info = containers.Map();
+    fields_info = struct();
 
     % Merged dataset to output, initially empty
-    output = containers.Map();
-    
+    output = struct();
+
+    % Get clusters field from parameters
+    clusters_field = p.Results.clusters_field;
+
     % Set of fields
     fields_set = p.Results.fields;
-    
+
     % If the clusters_field parameter is given, make sure it exists in the
     % set of fields
-    if (numel(p.Results.clusters_field) > 0) && ...
-            ~any(ismember(fields_set, p.Results.clusters_field))
-        fields_set = [fields_set, p.Results.clusters_field];
+    if (numel(clusters_field) > 0) && ...
+            ~any(ismember(fields_set, clusters_field))
+        fields_set = [fields_set, clusters_field];
     end;
-    
+
     % Cycle through data items
-    for dt = datasets
-        if iscell(dt{:})
-            disp('is cell');
-        else
-            disp('is not cell');
+    for dtc = datasets
+
+        % Number of elements in the current item
+        nelems_i = NaN;
+
+        % Check if current element is a struct
+        dt = dtc{:};
+        if ~isstruct(dt)
+            error('One or more items in `datasets` are not structs.');
         end;
+
+        % Cycle through fields for the current item
+        for fld = fields_set
+
+            if ~isfield(dt, fld{:})
+                error(['Data item does not contain required field `', ...
+                    fld{:} ,'`']);
+            elseif strcmp(fld{:}, clusters_field) && ~isinteger(dt.(clusters_field))
+                error(['`', clusters_field, '` must contain integer types']);
+            end;
+
+            % Get the field value
+            value = dt.(fld{:});
+
+            % Number of elements in field value
+            nelems_tmp = size(value, 1);
+
+            % Check the number of elements in the field value
+            if isnan(nelems_i)
+
+                % First field: get number of elements in value (must be the same
+                % for the remaining field values)
+                nelems_i = nelems_tmp;
+
+            elseif nelems_tmp ~= nelems_i
+
+                % Fields values after the first must have the same number of
+                % elements
+                error(['Data item contains fields with different sizes (', ...
+                    nelems_tmp, ' ~= ', nelems_i]);
+
+            end;
+
+            % Get/check info about the field value type
+            if ~isfield(fields_info, fld{:})
+
+                % If it's the first time this field appears, just get the info
+                fields_info.(fld{:}) = struct(...
+                    'type', typeof(value), ...
+                    'ncol', size(value, 2));
+
+            else
+
+                % If this field already appeared in previous data items, get the
+                % info and check/determine its compatibility with respect to
+                % previous data items
+                if size(value, 2) ~= fields_info.(fld{:}).ncol
+                    % Number of columns must be the same
+                    error(['Dimension mismatch in field `', fld{:},'`'])
+                end;
+
+                % Get the common supertype
+                fields_info.(fld{:}).type = common_supertype(...
+                    typeof(value), ...
+                    fields_info.(fld{:}).type);
+
+            end;
+
+        end;
+
+        % Update total number of elements
+        nelems = nelems + nelems_i;
+
     end;
 
 
-end % function
+end % function clumerge()
+
+function t = typeof(input)
+
+    if islogical(input)
+        t = 'logical';
+    elseif isinteger(input)
+        t = 'integer';
+    elseif isfloat(input)
+        t = 'float';
+    else
+        error(['Unsupported type in input: ', input]);
+    end;
+
+end % function typeof()
+
+function i = type_weight(type)
+
+    if strcmp(type, 'logical')
+        i = 1;
+    elseif strcmp(type, 'integer')
+        i = 2;
+    elseif strcmp(type, 'float')
+        i = 3;
+    else
+        error(['Unsupported type `', type, '`']);
+    end;
+
+end % function type_weight()
+
+function t = common_supertype(type1, type2)
+
+    t1w = type_weight(type1);
+    t2w = type_weight(type2);
+
+    if t1w >= t2w
+        t = type1;
+    else
+        t = type2;
+    end;
+
+end % function common_supertype()
 
 % Copyright (c) 2012-2023 Nuno Fachada
 % Distributed under the MIT License (See accompanying file LICENSE or copy
